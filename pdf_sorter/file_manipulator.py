7adeec6f3e3f2f31533f44dcd921483f57f5c12b
ChristophWenk <swissarmyknife@gmx.ch>
2022-07-19 20:41:25 +0200

    Logs formatting

--- pdf_sorter/file_manipulator.py
+++ pdf_sorter/file_manipulator.py
@@ -17,13 +17,13 @@ def rename_file(path, old_name, config):
     except KeyError as exception:
         logging.warning("Expected Token named " + exception.args[0] + " but it was not defined. Skipping rename.")
 
-    logger.info("Renaming file to..." + new_name)
+    logger.info("Renaming file to... " + new_name)
     if settings.dry_run is False:
         os.rename(path + "/" + old_name, path + "/" + new_name)
     return new_name
 
 
 def move_file(source_directory, file, target_directory):
-    logger.info("Moving file to..." + target_directory)
+    logger.info("Moving file to... " + target_directory)
     if settings.dry_run is False:
         shutil.move(source_directory + "/" + file, target_directory + "/" + file)
