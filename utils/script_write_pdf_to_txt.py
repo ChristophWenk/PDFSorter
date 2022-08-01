23d3243b34e3f608bbba8a913d485da371b058b9
ChristophWenk <swissarmyknife@gmx.ch>
2022-08-01 13:32:41 +0200

    Sanitize abbreviations

--- utils/script_write_pdf_to_txt.py
+++ utils/script_write_pdf_to_txt.py
@@ -4,7 +4,7 @@ from os import makedirs
 from pdf_sorter import pdf_parser
 
 if __name__ == '__main__':
-    file_name = ""
+    file_name = "EasyRide_2021-09-16_8d9vxtMSbb8kzrZS41"
     pdf_file_path = "../resources/test_files/" + file_name + ".pdf"
     pdf_text = pdf_parser.read_pdf(pdf_file_path)
 
