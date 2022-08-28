3d92f6d9765aa1178cacefcb8606cc86834438af
ChristophWenk <swissarmyknife@gmx.ch>
2022-08-28 12:43:54 +0200

    Fixed logger displaying wrong page number

--- pdf_sorter/pdf_parser.py
+++ pdf_sorter/pdf_parser.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 def print_metadata(pdf_file):
     # printing number of pages in pdf file
-    logger.debug("Total number of pages: " + pdf_file.numPages.__str__())
+    logger.debug("Total number of pages: " + str(pdf_file.numPages))
     return pdf_file.numPages
 
 
@@ -21,7 +21,7 @@ def read_pdf(file_path):
             pdf_page = pdf_file.getPage(i)
             pdf_text += pdf_page.extractText()
         except IndexError:
-            logger.warning("Page number " + i.__str__() + " could not be read. Skipping page.")
+            logger.warning("Page number " + str(i + 1) + " could not be read. Skipping page.")
         finally:
             i += 1
     return pdf_text
